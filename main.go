c7ed9168d664f70c4e1652dce090cd8c32871e76
minzus@hotmail.com <minzus@hotmail.com>
2019-03-23 17:38:46 -0700

    implemented ws and client

--- main.go
+++ main.go
@@ -4,19 +4,67 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page")
 }
 
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World Testing")
+func wsEndpointHandler(w http.ResponseWriter, r *http.Request) {
+
+	// workaround to avoid CORS error
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade to a websocket connection
+	// returns a pointer to a websocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("Client Connected.")
+
+	err = ws.WriteMessage(1, []byte("Hi Client!"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reader(ws)
+}
+
+// listens to incoming msg to websocket endpoint
+func reader(conn *websocket.Conn) {
+	for {
+
+		// read in msg to buffer
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// print read msg
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
+	http.HandleFunc("/", homePageHandler)
+	http.HandleFunc("/ws", wsEndpointHandler)
+}
+
+// websocket upgrader to hold Read/Write buffer size
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
 func main() {

67293b89fcb4677b01f61fc17dd781e9937d34eb
minzus@hotmail.com <minzus@hotmail.com>
2019-03-26 19:06:43 -0700

    hub dub

--- main.go
+++ main.go
@@ -1,9 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var addr = flag.String("addr", ":8080", "http service address")
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func newHub() {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+func serveHOME(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Nawt Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method Nawt Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
 func main() {
-	fmt.Println("hello world!")
+	flag.Parse()
+
+	// fmt.Println("hello world!")
 }
